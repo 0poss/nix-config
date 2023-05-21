fdd25db4c2814587d963bfc2a77b97c787f75f68
0poss <67114640+0poss@users.noreply.github.com>
2023-05-21 23:32:22 +0200

    Add i3status-rs

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
