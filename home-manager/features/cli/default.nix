f384132017b56d97c933e6207bfdaa6478fa6229
0poss <brnnrlxndr@gmail.com>
2023-07-03 00:03:24 +0200

    Update

--- home-manager/features/cli/default.nix
+++ home-manager/features/cli/default.nix
@@ -3,10 +3,12 @@
     ./bat.nix
     ./git.nix
     ./pfetch.nix
+    ./zsh.nix
   ];
 
   home.packages = with pkgs; [
     ripgrep # Better grep
     fd # Better find
+    fzf # Fuzzy finder
   ];
 }
