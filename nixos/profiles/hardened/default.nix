a3215b510ed9915338761d820d5720beac8d2b56
0poss <brnnrlxndr@gmail.com>
2023-07-09 23:50:15 +0200

    nixos/profiles/hardened : add kernel compilation options

--- nixos/profiles/hardened/default.nix
+++ nixos/profiles/hardened/default.nix
@@ -4,7 +4,14 @@ with lib;
 
 {
   boot = {
-    kernelPackages = pkgs.linuxPackages_6_1_hardened;
+
+    # Basically linuxPackages_X_X_hardened but with ignoreConfigErrors = true and compiled with clang.
+    kernelPackages = pkgs.linuxPackagesFor (
+      pkgs.linux_6_1_hardened.override {
+        #stdenv = pkgs.llvmPackages_11.stdenv;
+        ignoreConfigErrors = true;
+      });
+
     kernelParams = [
       "slub_debug=FZP"
       "slab_nomerge"
@@ -20,6 +27,7 @@ with lib;
       "quiet"
       "loglevel=0"
     ];
+
     kernel.sysctl = {
       "kernel.printk" = mkOverride 500 "3 3 3 3";
       "kernel.kptr_restrict" = mkOverride 500 2;
@@ -34,6 +42,40 @@ with lib;
       "net.ipv4.tcp_fack" = mkDefault 0;
       "kernel.yama.ptrace_scope" = mkOverride 500 2;
     };
+
+    kernelPatches = lib.mkForce [
+      {
+        name = "oposs";
+        patch = null;
+        extraStructuredConfig = with lib.kernel; {
+          WERROR = lib.mkForce yes;
+          X86_KERNEL_IBT = lib.mkForce yes;
+          DEBUG_VIRTUAL = lib.mkForce yes;
+          INIT_ON_ALLOC_DEFAULT_ON = lib.mkForce yes;
+          ZERO_CALL_USED_REGS = lib.mkForce yes;
+          RANDSTRUCT_PERFORMANCE = lib.mkForce yes;
+          INIT_ON_FREE_DEFAULT_ON = lib.mkForce yes;
+          EFI_DISABLE_PCI_DMA = lib.mkForce yes;
+          RESET_ATTACK_MITIGATION = lib.mkForce yes;
+          UBSAN = lib.mkForce yes;
+          UBSAN_BOUNDS = lib.mkForce yes;
+          UBSAN_LOCAL_BOUNDS = lib.mkForce yes;
+          USBAN_TRAP = lib.mkForce yes;
+          UBSAN_SANITIZE_ALL = lib.mkForce yes;
+          CFI_CLANG = lib.mkForce yes;
+          IOMMU_DEFAULT_DMA_STRUCT = lib.mkForce yes;
+          INTEL_IOMMU_DEFAULT_ON = lib.mkForce yes;
+          SLS = lib.mkForce yes;
+          INTEL_IOMMU_SVM = lib.mkForce yes;
+          BINFMT_MISC = lib.mkForce no;
+          KEXEC = lib.mkForce no;
+          HIBERNATION = lib.mkForce no;
+          COMPAT = lib.mkForce no;
+          IA32_EMULATION = lib.mkForce no;
+          X86_MSR = lib.mkForce no;
+        };
+      }
+    ];
   };
 
   systemd.coredump.enable = false;
