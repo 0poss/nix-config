128095450e18c67ba5b40065436952404b8be247
0poss <67114640+0poss@users.noreply.github.com>
2023-10-20 01:22:44 +0200

    Add home-manager NixOS module

--- nixos/hosts/base/default.nix
+++ nixos/hosts/base/default.nix
@@ -1,18 +1,13 @@
-{ lib, inputs, overlays, homeConfFiles, ... }:
+{ lib, ... }:
 
 with lib;
 
 {
   imports = [
-    inputs.home-manager.nixosModules.home-manager
     ./locale.nix
     ./zsh.nix
   ];
 
-  home-manager = {
-    extraSpecialArgs = { inherit inputs homeConfFiles overlays; };
-    users.oposs = homeConfFiles.homes.oposs;
-  };
   security.pam.services.swaylock = {};
 
   users.users.root.hashedPassword = "$6$rounds=50000000$cvIEZAR5IvtCciec$s2or9o8yAwnPO2gJmTE78Av3NJJRYXSsfBi1Rnf0IzU/0NsYENzDhBvszqWs2wZeEZ2qENawAMbjbbXVxvdwJ.";
