ce6df91a1a34da1b7d08599e2ca15fb88872d2b3
0poss <brnnrlxndr@gmail.com>
2023-10-21 16:18:54 +0200

    Add backlight to i3status + cleanup

--- home-manager/features/desktop/apps/i3status-rust.nix
+++ home-manager/features/desktop/apps/i3status-rust.nix
@@ -1,11 +1,19 @@
-{ pkgs, config, ... }:
+{ pkgs, lib, config, ... }:
 let
   inherit (config.colorscheme) colors;
+  has-backlight = config.programs.i3status-rust.backlight;
 in
 {
   imports = [ ];
 
-  programs.i3status-rust = {
+  options.programs.i3status-rust.backlight = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "Enable backlight block in the default bar.";
+    example = "true";
+  };
+
+  config.programs.i3status-rust = {
     enable = true;
     package = pkgs.unstable.i3status-rust;
     bars = {
@@ -17,32 +25,52 @@ in
             idle_fg = "#${colors.base05}";
           };
         };
-        blocks = let
-          notify = "notify-send -t 7500";
-        in
+        blocks =
+          let
+            notify = "notify-send -t 7500";
+          in
           [
             {
               block = "menu";
               text = " Nix ";
               items = [
-                { display = " Collect garbage ";
-                  cmd = ''${notify} "$(nix-collect-garbage -d 2>&1 | tail -n 2)"''; }
-                { display = " Optimize store ";
-                  cmd = ''${notify} "$(nix-store --optimise 2>&1 | tail -n 2)"''; }
+                {
+                  display = " Collect garbage ";
+                  cmd = ''${notify} "$(nix-collect-garbage -d 2>&1 | tail -n 2)"'';
+                }
+                {
+                  display = " Optimize store ";
+                  cmd = ''${notify} "$(nix-store --optimise 2>&1 | tail -n 2)"'';
+                }
               ];
             }
+
             {
               block = "external_ip";
               format = " $ip $country_code ";
             }
+
             { block = "cpu"; }
+
+          ] ++
+
+          lib.optionals has-backlight [
+            {
+              block = "backlight";
+              device = "intel_backlight";
+            }
+          ]
+
+          ++ [
+
             {
               block = "memory";
               format = " $icon $mem_used_percents.eng(w:2) ";
               format_alt = " $icon_swap $swap_used_percents.eng(w:2) ";
             }
+
             {
-             block = "disk_space";
+              block = "disk_space";
               path = "/";
               info_type = "available";
               alert_unit = "GB";
@@ -55,12 +83,14 @@ in
                 { button = "left"; update = true; }
               ];
             }
+
             {
               block = "time";
               interval = 5;
               format = {
                 full = " $timestamp.datetime(f:'%a %Y-%m-%d %R %Z', l:en_US) ";
-                short = " $timestamp.datetime(f:%R) "; };
+                short = " $timestamp.datetime(f:%R) ";
+              };
             }
           ];
       };
--- home-manager/features/desktop/wm/sway/default.nix
+++ home-manager/features/desktop/wm/sway/default.nix
@@ -15,87 +15,95 @@ in
 
   services.emacs.enable = true;
 
-  wayland.windowManager.sway = {
-    enable = true;
+  wayland.windowManager.sway =
+    let
+      mod = config.wayland.windowManager.sway.config.modifier;
+      menu_prompt =
+        "system: [e]xit [l]ock [r]eboot [p]oweroff [h]ibernate";
+      menu_mode = {
+        e = "exec swaymsg exit";
+        l = "exec swaylock";
+        r = "exec reboot";
+        p = "exec poweroff";
+        h = "systemctl hibernate";
+        Return = "mode default";
+        Escape = "mode default";
+        "${mod}+g" = "mode default";
+      };
+    in
+    {
+      enable = true;
 
-    wrapperFeatures = {
-      base = false;
-      gtk = false;
-    };
-
-    config = {
-      terminal = "kitty";
-      menu = "kickoff";
-
-      modifier = "Mod4";
-      left = "h";
-      down = "j";
-      up = "k";
-      right = "l";
-
-      startup = [
-        { command = "mako"; always = true; }
-      ];
-
-      input = {
-        "*" = {
-          xkb_layout = config.home.keyboard.layout;
-        };
+      wrapperFeatures = {
+        base = false;
+        gtk = false;
       };
 
-      output = {
-        "*" = {
-          bg = ''"'' + (builtins.toString config.selectedWallpaper) + ''" fill'';
-        };
-      };
+      config = {
+        terminal = "kitty";
+        menu = "kickoff";
 
-      gaps = {
-        inner = 10;
-      };
+        modifier = "Mod4";
+        left = "h";
+        down = "j";
+        up = "k";
+        right = "l";
 
-      colors = {
-        focused = {
-          background = "#${colors.base00}";
-          border = "#${colors.base01}";
-          text = "#${colors.base05}";
-          indicator = "#${colors.base09}";
-          childBorder = "#${colors.base0C}";
-        };
-      };
+        startup = [
+          { command = "mako"; always = true; }
+        ];
 
-      bars = [
-        {
-          statusCommand =
-            "i3status-rs ~/.config/i3status-rust/config-default.toml";
-          position = "bottom";
-          colors = {
-            background = "#${colors.base00}";
-            statusline = "#${colors.base00}";
-            separator = "#${colors.base05}";
-            focusedWorkspace = {
-              background = "#${colors.base00}";
-              border = "#${colors.base02}";
-              text = "#${colors.base05}";
-            };
+        input = {
+          "*" = {
+            xkb_layout = config.home.keyboard.layout;
           };
-        }
-      ];
+        };
 
-      modes =
-        let mod = config.wayland.windowManager.sway.config.modifier;
+        output = {
+          "*" = {
+            bg = ''"'' + (builtins.toString config.selectedWallpaper) + ''" fill'';
+          };
+        };
+
+        gaps = {
+          inner = 10;
+        };
+
+        colors = {
+          focused = {
+            background = "#${colors.base00}";
+            border = "#${colors.base01}";
+            text = "#${colors.base05}";
+            indicator = "#${colors.base09}";
+            childBorder = "#${colors.base0C}";
+          };
+        };
+
+        bars = [
+          {
+            statusCommand =
+              "i3status-rs ~/.config/i3status-rust/config-default.toml";
+            position = "bottom";
+            colors = {
+              background = "#${colors.base00}";
+              statusline = "#${colors.base00}";
+              separator = "#${colors.base05}";
+              focusedWorkspace = {
+                background = "#${colors.base00}";
+                border = "#${colors.base02}";
+                text = "#${colors.base05}";
+              };
+            };
+          }
+        ];
+
+        modes =
+          let
             inherit (config.wayland.windowManager.sway.config)
               left down up right;
-        in
-          {
-            "system: [e]xit [r]eboot [p]oweroff [h]ibernate" = {
-              e = "exec swaymsg exit";
-              r = "exec reboot";
-              p = "exec poweroff";
-              h = "systemctl hibernate";
-              Return = "mode default";
-              Escape = "mode default";
-              "${mod}+g" = "mode default";
-            };
+          in
+            {
+              "${menu_prompt}" = menu_mode;
             resize = {
               "${mod}+${left}" = "resize shrink width 10 px";
               "${mod}+${down}" = "resize grow height 10 px";
@@ -106,80 +114,86 @@ in
               "${mod}+g" = "mode default";
             };
           };
-      
-      keybindings = with lib;
-        let mod = config.wayland.windowManager.sway.config.modifier;
+
+        keybindings = with lib;
+          let
+            mod = config.wayland.windowManager.sway.config.modifier;
             inherit (config.wayland.windowManager.sway.config)
               terminal menu left down up right;
-        in
-          let ws = if "fr" == config.home.keyboard.layout
-                   then { k1 = "ampersand";
-                          k2 = "eacute";
-                          k3 = "quotedbl";
-                          k4 = "apostrophe";
-                          k5 = "parenleft";
-                          k6 = "egrave";
-                          k7 = "minus";
-                          k8 = "underscore";
-                          k9 = "ccedilla";
-                          k10 = "agrave"; }
-                   else listToAttrs
-                     (map (k: nameValuePair (toString k) (toString (trivial.modulo k 10)))
-                       (range 1 10));
           in
-            {
-              "${mod}+Shift+b" = "reload";
+          let
+            ws =
+              if "fr" == config.home.keyboard.layout
+              then {
+                k1 = "ampersand";
+                k2 = "eacute";
+                k3 = "quotedbl";
+                k4 = "apostrophe";
+                k5 = "parenleft";
+                k6 = "egrave";
+                k7 = "minus";
+                k8 = "underscore";
+                k9 = "ccedilla";
+                k10 = "agrave";
+              }
+              else
+                listToAttrs
+                  (map (k: nameValuePair (toString k) (toString (trivial.modulo k 10)))
+                    (range 1 10));
+          in
+          {
+            "${mod}+Shift+b" = "reload";
 
-              "${mod}+Return" = "exec ${terminal}";
-              "${mod}+e" = "exec emacsclient --create-frame";
-              "${mod}+d" = "exec ${menu}";
-              "${mod}+Shift+q" = "kill";
+            "${mod}+Return" = "exec ${terminal}";
+            "${mod}+e" = "exec emacsclient --create-frame";
+            "${mod}+d" = "exec ${menu}";
+            "${mod}+Shift+q" = "kill";
 
-              "${mod}+o" = "split v";
-              "${mod}+p" = "split h";
-              "${mod}+f" = "fullscreen";
-              "${mod}+Shift+space" = "floating toggle";
-              "${mod}+Shift+e" = "layout toggle split";
-              "${mod}+Shift+s" = "layout stacking";
-              "${mod}+Shift+w" = "layout tabbed";
-              "${mod}+r" = "mode resize";
-              "${mod}+Shift+v" = ''mode "system: [e]xit [r]eboot [p]oweroff [h]ibernate"'';
+            "${mod}+o" = "split v";
+            "${mod}+p" = "split h";
+            "${mod}+f" = "fullscreen";
+            "${mod}+Shift+space" = "floating toggle";
+            "${mod}+Shift+e" = "layout toggle split";
+            "${mod}+Shift+s" = "layout stacking";
+            "${mod}+Shift+w" = "layout tabbed";
+            "${mod}+r" = "mode resize";
+            "${mod}+Shift+v" = ''mode "${menu_prompt}"'';
 
-              "${mod}+${left}" = "focus left";
-              "${mod}+${down}" = "focus down";
-              "${mod}+${up}" = "focus up";
-              "${mod}+${right}" = "focus right";
-              "${mod}+Shift+${left}" = "move left";
-              "${mod}+Shift+${down}" = "move down";
-              "${mod}+Shift+${up}" = "move up";
-              "${mod}+Shift+${right}" = "move right";
+            "${mod}+${left}" = "focus left";
+            "${mod}+${down}" = "focus down";
+            "${mod}+${up}" = "focus up";
+            "${mod}+${right}" = "focus right";
+            "${mod}+Shift+${left}" = "move left";
+            "${mod}+Shift+${down}" = "move down";
+            "${mod}+Shift+${up}" = "move up";
+            "${mod}+Shift+${right}" = "move right";
 
-              "${mod}+${ws.k1}" =  "workspace 1";
-              "${mod}+${ws.k2}" =  "workspace 2";
-              "${mod}+${ws.k3}" =  "workspace 3";
-              "${mod}+${ws.k4}" =  "workspace 4";
-              "${mod}+${ws.k5}" =  "workspace 5";
-              "${mod}+${ws.k6}" =  "workspace 6";
-              "${mod}+${ws.k7}" =  "workspace 7";
-              "${mod}+${ws.k8}" =  "workspace 8";
-              "${mod}+${ws.k9}" =  "workspace 9";
-              "${mod}+${ws.k10}" = "workspace 10";
+            "${mod}+${ws.k1}" = "workspace 1";
+            "${mod}+${ws.k2}" = "workspace 2";
+            "${mod}+${ws.k3}" = "workspace 3";
+            "${mod}+${ws.k4}" = "workspace 4";
+            "${mod}+${ws.k5}" = "workspace 5";
+            "${mod}+${ws.k6}" = "workspace 6";
+            "${mod}+${ws.k7}" = "workspace 7";
+            "${mod}+${ws.k8}" = "workspace 8";
+            "${mod}+${ws.k9}" = "workspace 9";
+            "${mod}+${ws.k10}" = "workspace 10";
 
-              "${mod}+Shift+${ws.k1}" = "move container to workspace number 1";
-              "${mod}+Shift+${ws.k2}" = "move container to workspace number 2";
-              "${mod}+Shift+${ws.k3}" = "move container to workspace number 3";
-              "${mod}+Shift+${ws.k4}" = "move container to workspace number 4";
-              "${mod}+Shift+${ws.k5}" = "move container to workspace number 5";
-              "${mod}+Shift+${ws.k6}" = "move container to workspace number 6";
-              "${mod}+Shift+${ws.k7}" = "move container to workspace number 7";
-              "${mod}+Shift+${ws.k8}" = "move container to workspace number 8";
-              "${mod}+Shift+${ws.k9}" = "move container to workspace number 9";
-              "${mod}+Shift+${ws.k10}" = "move container to workspace number 10";
-            };
-      defaultWorkspace = "workspace number 1";
-    };
+            "${mod}+Shift+${ws.k1}" = "move container to workspace number 1";
+            "${mod}+Shift+${ws.k2}" = "move container to workspace number 2";
+            "${mod}+Shift+${ws.k3}" = "move container to workspace number 3";
+            "${mod}+Shift+${ws.k4}" = "move container to workspace number 4";
+            "${mod}+Shift+${ws.k5}" = "move container to workspace number 5";
+            "${mod}+Shift+${ws.k6}" = "move container to workspace number 6";
+            "${mod}+Shift+${ws.k7}" = "move container to workspace number 7";
+            "${mod}+Shift+${ws.k8}" = "move container to workspace number 8";
+            "${mod}+Shift+${ws.k9}" = "move container to workspace number 9";
+            "${mod}+Shift+${ws.k10}" = "move container to workspace number 10";
+          };
+        defaultWorkspace = "workspace number 1";
+      };
 
-    extraConfig = ''
+      extraConfig = ''
     '';
-  };
+    };
 }
