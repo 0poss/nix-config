8c2cdb4a5bfc436fa8aed915e76a84033259c2a5
0poss <67114640+0poss@users.noreply.github.com>
2023-10-22 16:09:14 +0200

    Change font

--- home-manager/features/fonts.nix
+++ home-manager/features/fonts.nix
@@ -3,13 +3,13 @@ let
   mkFontOption = kind: {
     family = lib.mkOption {
       type = lib.types.str;
-      default = "ProggyClean Nerd Font";
+      default = "Iosevka Nerd Font";
       description = "Family name for ${kind} font profile";
       example = "Fira Code";
     };
     package = lib.mkOption {
       type = lib.types.package;
-      default = pkgs.nerdfonts.override { fonts = [ "ProggyClean" "Iosevka" "Terminus" ]; };
+      default = pkgs.nerdfonts.override { fonts = [ "Iosevka" ]; };
       description = "Package for ${kind} font profile";
       example = "pkgs.fira-code";
     };
