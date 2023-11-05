4a1c1528b767a1daf0655c1932825ea7b71eb6a5
0poss <brnnrlxndr@gmail.com>
2023-11-05 18:30:03 +0100

    Update and improve hardening

--- nixos/profiles/hardened/default.nix
+++ nixos/profiles/hardened/default.nix
@@ -1,16 +1,62 @@
 { pkgs, lib, ... }:
-
-with lib;
-
+let
+  # TODO : make it easily available in the flake.
+  mkHardOpt = lib.mkOverride 99;
+in
 {
   boot = {
 
-    # Basically linuxPackages_X_X_hardened but with ignoreConfigErrors = true and compiled with clang.
-    kernelPackages = pkgs.linuxPackagesFor (
-      pkgs.linux_6_1_hardened.override {
-        stdenv = pkgs.llvmPackages_11.stdenv;
+    kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_6_5_hardened.override
+      {
+        #stdenv = pkgs.ccacheStdenv;
+        autoModules = true;
         ignoreConfigErrors = true;
       });
+
+    kernelPatches = [
+      {
+        name = "oposs-hardening";
+        patch = null;
+        extraStructuredConfig =
+          let
+            hye = mkHardOpt lib.kernel.yes;
+            hno = mkHardOpt lib.kernel.no;
+          in
+          {
+            EXPERT = hye;
+
+            TRIM_UNUSED_KSYMS = hye;
+            XFS_FS = hno;
+            AIO = lib.mkOverride 99 lib.kernel.no;
+            INPUT_EVBUG = hno;
+            FTRACE = hno;
+            KPROBES = hno;
+            EFI_CUSTOM_SSDT_OVERLAYS = hno;
+            #FB = hno;
+            STAGING = hno;
+            COREDUMP = hno;
+
+            AF_KCM = hno; # necessary to disable BPF_SYSCALL
+            BPF_SYSCALL = hno;
+
+            DLM = hno; # necessary to disable SCTP
+            IP_SCTP = hno;
+            IP_DCCP = hno;
+
+            ACPI_TABLE_UPGRADE = hno;
+            X86_IOPL_IOPERM = hno;
+            X86_CPUID = hno;
+            USER_NS = hno;
+            KEXEC_CORE = hno;
+            KEXEC = hno;
+            KEXEC_FILE = hno;
+            MAGIC_SYSRQ = hno;
+            KALLSYMS = hno;
+            KSM = hno;
+            HIBERNATION = hno;
+          };
+      }
+    ];
 
     kernelParams = [
       "slub_debug=FZP"
@@ -29,66 +75,32 @@ with lib;
     ];
 
     kernel.sysctl = {
-      "kernel.printk" = mkOverride 500 "3 3 3 3";
-      "kernel.kptr_restrict" = mkOverride 500 2;
-      "kernel.ftrace_enabled" = mkDefault false;
-      "kernel.core.bpf_jit_enable" = mkDefault true;
-      "dev.tty.ldisc_autoload" = mkDefault 0;
-      "net.ipv4.tcp_rfc1337" = mkDefault 1;
-      "net.ipv4.icmp_echo_ignore_all" = mkDefault 1;
-      "net.ipv6.icmp_echo_ignore_all" = mkDefault 1;
-      "net.ipv4.tcp_sack" = mkDefault 0;
-      "net.ipv4.tcp_dsack" = mkDefault 0;
-      "net.ipv4.tcp_fack" = mkDefault 0;
-      "kernel.yama.ptrace_scope" = mkOverride 500 2;
+      "kernel.printk" = mkHardOpt "3 3 3 3";
+      "kernel.kptr_restrict" = mkHardOpt 2;
+      "kernel.ftrace_enabled" = mkHardOpt false;
+      "kernel.core.bpf_jit_enable" = mkHardOpt true;
+      "dev.tty.ldisc_autoload" = mkHardOpt 0;
+      "net.ipv4.tcp_rfc1337" = mkHardOpt 1;
+      "net.ipv4.icmp_echo_ignore_all" = mkHardOpt 1;
+      "net.ipv6.icmp_echo_ignore_all" = mkHardOpt 1;
+      "net.ipv4.tcp_sack" = mkHardOpt 0;
+      "net.ipv4.tcp_dsack" = mkHardOpt 0;
+      "net.ipv4.tcp_fack" = mkHardOpt 0;
+      "kernel.yama.ptrace_scope" = mkHardOpt 2;
     };
-
-    kernelPatches = lib.mkForce [
-      {
-        name = "oposs";
-        patch = null;
-        extraStructuredConfig = with lib.kernel; {
-          WERROR = lib.mkForce yes;
-          X86_KERNEL_IBT = lib.mkForce yes;
-          DEBUG_VIRTUAL = lib.mkForce yes;
-          INIT_ON_ALLOC_DEFAULT_ON = lib.mkForce yes;
-          ZERO_CALL_USED_REGS = lib.mkForce yes;
-          RANDSTRUCT_PERFORMANCE = lib.mkForce yes;
-          INIT_ON_FREE_DEFAULT_ON = lib.mkForce yes;
-          EFI_DISABLE_PCI_DMA = lib.mkForce yes;
-          RESET_ATTACK_MITIGATION = lib.mkForce yes;
-          UBSAN = lib.mkForce yes;
-          UBSAN_BOUNDS = lib.mkForce yes;
-          UBSAN_LOCAL_BOUNDS = lib.mkForce yes;
-          USBAN_TRAP = lib.mkForce yes;
-          UBSAN_SANITIZE_ALL = lib.mkForce yes;
-          CFI_CLANG = lib.mkForce yes;
-          IOMMU_DEFAULT_DMA_STRUCT = lib.mkForce yes;
-          INTEL_IOMMU_DEFAULT_ON = lib.mkForce yes;
-          SLS = lib.mkForce yes;
-          INTEL_IOMMU_SVM = lib.mkForce yes;
-          BINFMT_MISC = lib.mkForce no;
-          KEXEC = lib.mkForce no;
-          HIBERNATION = lib.mkForce no;
-          COMPAT = lib.mkForce no;
-          IA32_EMULATION = lib.mkForce no;
-          X86_MSR = lib.mkForce no;
-        };
-      }
-    ];
   };
 
   systemd.coredump.enable = false;
 
   security = {
     apparmor = {
-      enable = mkDefault true;
-      killUnconfinedConfinables = mkDefault true;
+      enable = mkHardOpt true;
+      killUnconfinedConfinables = mkHardOpt true;
     };
 
-    lockKernelModules = mkDefault true;
-    protectKernelImage = mkDefault true;
-    forcePageTableIsolation = mkDefault true;
-    virtualisation.flushL1DataCache = mkDefault "always";
+    lockKernelModules = mkHardOpt true;
+    protectKernelImage = mkHardOpt true;
+    forcePageTableIsolation = mkHardOpt true;
+    virtualisation.flushL1DataCache = mkHardOpt "always";
   };
 }
