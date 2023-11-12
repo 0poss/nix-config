51dd48964581dcd34b411f3f7b24da48842d30a8
0poss <67114640+0poss@users.noreply.github.com>
2023-11-12 14:58:28 +0100

    Switch to ungoogled-chromium

--- home-manager/features/desktop/apps/chromium.nix
+++ home-manager/features/desktop/apps/chromium.nix
@@ -4,14 +4,7 @@
 
   programs.chromium = {
     enable = true;
-    # TODO : https://discourse.nixos.org/t/home-manager-ungoogled-chromium-with-extensions/15214
-    #package = pkgs.ungoogled-chromium;
-    extensions = [
-      # umatrix
-      { id = "ogfcmafjalglgifnmanfmnieipoejdcf"; }
-      # vimium
-      { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; }
-    ];
+    package = pkgs.ungoogled-chromium;
     commandLineArgs = [
       # Disable JIT.
       "--js-flags=--jitless"
