2a90059c0404f3cce62feaece831612a8c458ae0
0poss <67114640+0poss@users.noreply.github.com>
2023-11-12 15:51:43 +0100

    Bump kickoff

--- pkgs/kickoff/default.nix
+++ pkgs/kickoff/default.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "kickoff";
-  version = "v0.7.0";
+  version = "v0.7.1";
 
   src = fetchFromGitHub {
     owner = "j0ru";
     repo = pname;
     rev = version;
-    sha256 = "sha256-AolJXFolMEwoK3AtC93naphZetytzRl1yI10SP9Rnzo=";
+    sha256 = "sha256-9QupKpB3T/6gdGSeLjRknjPdgOzbfzEeJreIamWwpSw=";
   };
 
-  cargoSha256 = "sha256-OEFCR/2zSVZhZqAp6n48UyIwplRXxKb9HENsVaLIKkM=";
+  cargoSha256 = "sha256-PEOvz3m8/9gsWfczfNYMqXU5+Ga3ylE036vUheY2LB0=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ fontconfig wayland libxkbcommon ];
