96565868307825058c1c4906eef1cd62b9d1bef7
0poss <67114640+0poss@users.noreply.github.com>
2023-12-01 00:07:44 +0100

    Update

--- nixos/hosts/puffy/default.nix
+++ nixos/hosts/puffy/default.nix
@@ -1,11 +1,15 @@
-{ inputs, homeConfFiles, overlays, pkgs, nixosConfFiles, ... }:
+{ lib, inputs, homeConfFiles, overlays, pkgs, nixosConfFiles, ... }:
 {
   imports = with nixosConfFiles; [
     inputs.home-manager.nixosModules.home-manager
-    hosts.base
     features.wayland
     features.nixpkgs
+    features.locale
   ];
+
+  services.logrotate.checkConfig = false;
+
+  users.users.root.hashedPassword = "$6$rounds=50000000$cvIEZAR5IvtCciec$s2or9o8yAwnPO2gJmTE78Av3NJJRYXSsfBi1Rnf0IzU/0NsYENzDhBvszqWs2wZeEZ2qENawAMbjbbXVxvdwJ.";
 
   programs.sway.enable = true;
 
@@ -22,7 +26,26 @@
     };
   };
 
+  nix.settings.substituters = [];
+  system.stateVersion = "23.05";
   nixpkgs.hostPlatform = "x86_64-linux";
+  nixpkgs.overlays = [
+    (let
+      # bootstrapTools' `glibc` does not provide enough PIE linking environment.
+      isGoodStdenv = stdenv: let
+        name = builtins.trace stdenv.name (stdenv.name or "");
+      in
+        !(lib.strings.hasInfix "bootstrap" name);
+    in
+      self: super: {
+        stdenv = super.stdenv // super.lib.optionalAttrs (isGoodStdenv super.stdenv) {
+          mkDerivation = fnOrAttrs: super.stdenv.mkDerivation (
+            if builtins.isFunction fnOrAttrs then fnOrAttrs
+            else fnOrAttrs // { hardeningEnable = [ "pie" ]; }
+          );
+        };
+      })
+  ];
 
   home-manager = {
     extraSpecialArgs = { inherit inputs homeConfFiles overlays; };
@@ -37,11 +60,12 @@
 
     packages = with pkgs; [
       git
-      binary-ninja
+      #binary-ninja
     ];
     shell = pkgs.zsh;
     hashedPassword = "$y$j9T$3ikX9sUneJBNeiJG5jdst1$2AAzLD2pkDVORF61mua5GtezE8du.07jaPCkvz0ytK8";
   };
+  programs.zsh.enable = true;
 
   networking.hostName = "puffy";
 }
