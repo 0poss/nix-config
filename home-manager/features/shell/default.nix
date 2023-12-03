2846fb45261eeb2722892578be02bc51a3f15cec
0poss <67114640+0poss@users.noreply.github.com>
2023-12-03 17:24:06 +0100

    Update

--- home-manager/features/shell/default.nix
+++ home-manager/features/shell/default.nix
@@ -2,10 +2,6 @@
   programs.zsh = {
     enable = true;
     defaultKeymap = "emacs";
-    syntaxHighlighting = {
-      enable = true;
-      highlighters = [ "brackets" "cursor" "line" ];
-    };
     localVariables = {
       PROMPT = "> ";
       RPROMPT = "%2~";
